8c42ca95e9609e185d123c504ae84b4bd3195c40
Nancy Jiang <nancy.jiang10@journalism.cuny.edu>
2026-05-04 15:11:08 -0400

    commit!

---
+++ src/lib/components/Layout/LandingGrid.svelte
@@ -0,0 +1,20 @@
+<script>
+  import CardGrid from '$lib/components/Data/CardGrid.svelte';
+  import Card from '$lib/components/Data/Card.svelte';
+  import { base } from '$app/paths';
+
+  let { items = [] } = $props();
+</script>
+
+<CardGrid>
+  {#each items as item (item.title)}
+    <Card href={`${base}/clips/${item.slug}`} image={item.image} imageAlt={item.imageAlt}>
+      <h3>{item.title}</h3>
+      <p>{item.description}</p>
+    </Card>
+  {/each}
+</CardGrid>
+
+<style lang="scss">
+  /* styles are inherited from CardGrid/Card */
+</style>
